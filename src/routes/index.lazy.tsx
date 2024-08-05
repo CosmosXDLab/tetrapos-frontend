6c5dc0baecfca034cdd98dcded05107f998f2f29
Bryan Grados <bryangrados9@gmail.com>
2024-08-05 13:24:20 -0500

    feat: customers module has available

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,19 +1,5 @@
-import { Button } from "@/components/ui/button";
-import routesNavigators from "@/config/navigations/routes";
-import randomKey from "@/utils/randomKey";
-import { Link, createLazyFileRoute } from "@tanstack/react-router";
+import { createLazyFileRoute } from '@tanstack/react-router'
 
-export const Route = createLazyFileRoute("/")({
-	component: () => (
-		<div className="flex flex-wrap items-start justify-start w-full h-full gap-10 p-12">
-			{routesNavigators.map(({ icon: Icon, name, path }) => (
-				<Button key={randomKey()} variant={"homebutton"} size={"card"} asChild>
-					<Link to={path} className="flex items-center gap-2">
-						<Icon className="fill-current" width={32} />
-						{name}
-					</Link>
-				</Button>
-			))}
-		</div>
-	),
-});
+export const Route = createLazyFileRoute('/')({
+  component: () => <div>Hello /!</div>
+})
